3f2d203658a5e28bd7852f62693cf8141f407cc4
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-08-25 21:43:17 +0800

    change usercontroller

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -176,7 +176,7 @@ class UserController extends Controller
             ]);
         }
 
-        $this->validate($request, $validateData);
+        $this->validate($request->validateData);
 
         $user->update($data);
 
