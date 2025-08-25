15e14f68f2b3dce2cac9202d6e811a872e7921ef
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-08-25 22:08:07 +0800

    change user register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -18,25 +18,47 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param  array<string, string>  $input
      */
-    public function create(array $input, Request $request): User
+    public function create( Request $request): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'username' => ['string', 'min:3', 'unique:users'],
-            'celuller_no' => ['string', 'min:11', 'unique:users'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
+
+        // Validator::make($input, [
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        //     'username' => ['string', 'min:3', 'unique:users'],
+        //     'celuller_no' => ['string', 'min:11', 'unique:users'],
+        //     'password' => $this->passwordRules(),
+        //     'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        // ])->validate();
 
 
-        $user =  User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'username' => $input['username'],
-            'celuller_no' => $input['celuller_no'],
-            'password' => Hash::make($input['password']),
+        // $user =  User::create([
+        //     'name' => $input['name'],
+        //     'email' => $input['email'],
+        //     'username' => $input['username'],
+        //     'celuller_no' => $input['celuller_no'],
+        //     'password' => Hash::make($input['password']),
+        // ]);
+
+        $this->validate($request, [
+            'name'                  => 'required|min:2',
+            'email'                 => 'required|email|unique:users,email',
+            'password'              => 'confirmed|min:8',
+            'password_confirmation' => 'required',
+            'username'              => ['string', 'min:3','unique:users'],
+            'celuller_no'           => ['string', 'min:11', 'max:12','unique:users'],
+            'terms'                 => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ]);
+
+        // Default data
+        $data = [
+            'name'        => $request->input('name'),
+            'email'       => $request->input('email'),
+            'username'    => $request->input('username'),
+            'celuller_no' => $request->input('celuller_no'),
+            'password'    => Hash::make($request->input('password')),
+        ];
+
+        $user = User::create($data);
 
         //assign role to user
         $user->syncRoles($request->input('roles'));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -71,7 +71,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }} <span class="text-danger">*</span></label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" {{ old('password') }}>
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -86,7 +86,7 @@
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}<span class="text-danger">*</span></label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" {{ old('password_confirmation') }}>
                                 </div>
                             </div>
                             <div class="mb-3 row" hidden>
